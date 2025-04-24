a8d158b0594d4018cdca3da859d37191323b0b68
Rudrabose <92892605+Rudrabose@users.noreply.github.com>
2025-04-24 16:03:38 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -58,10 +58,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(
-    chat_id=7504737201,                 # chat or user ID
-    document="myfile.pdf"      # path or file‑id of the document you want to send
-        )
+        await bot.send_document(x)
         await input.delete(True)    
         file_name, ext = os.path.splitext(os.path.basename(x))
 
